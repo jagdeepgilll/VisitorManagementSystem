350d8cbb1f61f1f19f21facacc1cf7c7424be054
Jagdeep Singh <JS127@atcltd.atc.org.nz>
2021-03-11 13:25:27 +1300

    Add project files.

---
+++ VisitorManagementSystem/Data/VisitorDbContext.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using VisitorManagementSystem.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+
+
+namespace VisitorManagementSystem.Data
+{
+    public class VisitorDbContext :IdentityDbContext
+    {
+        // Here we add in all the tables we are using.
+        public DbSet<StaffNames> StaffNames { get; set; }
+        public DbSet<Visitors> Visitors { get; set; }
+        // We need 2 constructors, one is empty, and the other injects in DbContextOptions
+        public VisitorDbContext(DbContextOptions<VisitorDbContext> options) : base(options)
+        {
+        }
+        public VisitorDbContext()
+        {
+        }
+    }
+}
---
+++ VisitorManagementSystem/Data/VisitorManagementSystemContext.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using VisitorManagementSystem.Models;
+
+namespace VisitorManagementSystem.Data
+{
+    public class VisitorManagementSystemContext : DbContext
+    {
+        public VisitorManagementSystemContext (DbContextOptions<VisitorManagementSystemContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<VisitorManagementSystem.Models.Visitors> Visitors { get; set; }
+
+        public DbSet<VisitorManagementSystem.Models.StaffNames> StaffNames { get; set; }
+    }
+}
